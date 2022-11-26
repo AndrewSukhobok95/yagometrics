e0994cc137621aa3355b4b84b6d51787a22189bb
AndrewSukhobok95 <suha1253516@gmail.com>
2022-11-26 11:06:24 +0200

    Fixed response statuses

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -23,27 +24,30 @@ func (mh MetricHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	fields := ParseURL(r.URL.Path)
 	if len(fields) != 4 {
-		http.Error(w, "Broken address.", http.StatusBadRequest)
+		http.Error(w, "Broken address.", http.StatusNotFound)
 		return
 	}
 	metricType := fields[1]
 	metricName := fields[2]
 	metricValueString := fields[3]
-	if metricType == "gauge" {
+	switch {
+	case metricType == "gauge":
 		metricValue, err := strconv.ParseFloat(metricValueString, 64)
 		if err != nil {
 			http.Error(w, "Broken address.", http.StatusBadRequest)
 			return
 		}
 		mh.storage.InsertGaugeMetric(metricName, metricValue)
-	}
-	if metricType == "counter" {
+	case metricType == "counter":
 		metricValue, err := strconv.ParseInt(metricValueString, 10, 64)
 		if err != nil {
 			http.Error(w, "Broken address.", http.StatusBadRequest)
 			return
 		}
 		mh.storage.AddCounterMetric(metricName, metricValue)
+	default:
+		http.Error(w, fmt.Sprintf("%s metric type is not implemented.", metricName), http.StatusNotImplemented)
+		return
 	}
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
