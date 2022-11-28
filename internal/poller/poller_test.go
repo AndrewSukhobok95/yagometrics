4582ef4eb97bcc7ce48c9b01de08983e4e5523ca
AndrewSukhobok95 <suha1253516@gmail.com>
2022-11-28 14:20:35 +0200

    [Increment 3] Added poller test

---
+++ internal/poller/poller_test.go
@@ -0,0 +1,39 @@
+package poller_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/AndrewSukhobok95/yagometrics.git/internal/poller"
+	"github.com/AndrewSukhobok95/yagometrics.git/internal/storage"
+)
+
+func TestPoll(t *testing.T) {
+	tests := []struct {
+		name             string
+		pollInterval     time.Duration
+		waitInterval     time.Duration
+		wantCounterValue int64
+	}{
+		{
+			name:             "Positive test: Counter metric",
+			pollInterval:     10 * time.Millisecond,
+			waitInterval:     100 * time.Millisecond,
+			wantCounterValue: 10,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			memStorage := storage.NewMemStorage()
+			go poller.Poll(memStorage, tt.pollInterval)
+			time.Sleep(tt.waitInterval)
+			value, err := memStorage.GetCounterMetric("PollCount")
+			if err != nil {
+				t.Errorf("Something wrong with the PollCount key")
+			}
+			if value != tt.wantCounterValue {
+				t.Errorf("Expected PollCount %d, got %d", tt.wantCounterValue, value)
+			}
+		})
+	}
+}
